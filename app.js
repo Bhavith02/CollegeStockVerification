dbea14f22d090c300def82189effa153c5e685af
Bhavith02 <bhavith070202@gmail.com>
2021-12-09 06:54:26 +0530

    added autentication

--- app.js
+++ app.js
@@ -10,18 +10,66 @@ app.use(express.static("images"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+mongoose.connect("mongodb://localhost:27017/stockDB",
+  {useNewUrlParser:true},{useUnifiedTopology:true}
+);
+const registerSchema = {
+  first_name:String,
+  last_name:String,
+  Designation:String,
+  AdminID:String,
+  email:String,
+  password:String,
+  contact:Number
+};
+
+const Register = mongoose.model("Register",registerSchema);
+
 app.get('/', function(req, res){
   res.render("index");
 });
-app.post('/adminlogin',function(req, res){
+app.get('/adminlogin',function(req, res){
   res.render("adminlogin");
 });
-app.post('/adminsignup',function(req, res){
+app.get('/adminsignup',function(req, res){
   res.render("adminsignup");
 });
-app.post('/others',function(req, res){
+app.get('/others',function(req, res){
   res.render("others");
 });
+app.post('/adminsignup',function(req, res){
+  const newRegister = new Register({
+    email: req.body.email,
+    password: req.body.password
+  });
+  newRegister.save(function (err){
+    if (err){
+      console.log(err);
+    }else{
+      res.redirect('/');
+    }
+  });
+});
+
+app.post('/adminlogin', function(req, res) {
+  const username = req.body.email;
+  const password = req.body.password;
+
+  Register.findOne({email:username},function(err,foundUser){
+    if (err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("navigation");
+        }
+      }
+    }
+  });
+});
+
+
+
 
 
 app.listen(3000,function(){
