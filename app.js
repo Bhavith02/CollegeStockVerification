788f8f2cc9969bd239cfad166e80b917da659b47
Bhavith02 <bhavith070202@gmail.com>
2021-12-14 18:26:18 +0530

    added admindetails page

--- app.js
+++ app.js
@@ -13,17 +13,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost:27017/stockDB",
   {useNewUrlParser:true},{useUnifiedTopology:true}
 );
-const registerSchema = {
-  first_name:String,
-  last_name:String,
-  Designation:String,
-  AdminID:String,
-  email:String,
-  password:String,
-  contact:Number
-};
+const registerSchema = new mongoose.Schema({
+  first_name:{ type : String, required : true, max : [127, "Max Length is 127 characters"]},
+  last_name:{ type : String, required : true, max : [127, "Max Length is 127 characters"]},
+  Designation:{type:String},
+  Admin_id:{type:String,required:true},
+  email:{type:String,required:true},
+  password:{type:String,required:true},
+  contact:{type:Number}
+});
 
 const Register = mongoose.model("Register",registerSchema);
+
 
 app.get('/', function(req, res){
   res.render("index");
@@ -37,10 +38,23 @@ app.get('/adminsignup',function(req, res){
 app.get('/others',function(req, res){
   res.render("others");
 });
+app.get('/admindetails',function(req, res){
+  Register.find({},function(err,admins){
+    res.render("admindetails",{
+      adminlist:admins
+    });
+  });
+});
 app.post('/adminsignup',function(req, res){
   const newRegister = new Register({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    Designation: req.body.Designation,
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
+    confirm_password: req.body.password,
+    Admin_id: req.body.Admin_id,
+    contact_no: req.body.contact_no
   });
   newRegister.save(function (err){
     if (err){
@@ -50,6 +64,7 @@ app.post('/adminsignup',function(req, res){
     }
   });
 });
+
 
 app.post('/adminlogin', function(req, res) {
   const username = req.body.email;
